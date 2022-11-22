87cdc1a7c3451366403eb43b3129149ffb546084
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-22 16:26:22 +0700

    navigation bar

--- YoutubeUI/YoutubeUI/ContentView.swift
+++ YoutubeUI/YoutubeUI/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
@@ -19,3 +18,43 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home : View {
+    var body: some View{
+        NavigationView{
+            Text("Hello ini content").navigationBarItems(
+                leading: HStack{
+                Button(
+                    action: {
+                        print("Hello button")}){
+                    Image("youtube").resizable().renderingMode(.original).frame(width: 90, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                },
+                trailing: HStack(spacing:20){
+                    Button(
+                        action: {
+                        print("Hello Print")
+                        }){
+                        Image(systemName: "tray.full")
+                    }
+                    Button(
+                        action: {
+                        print("Hello Print")
+                        }){
+                        Image(systemName: "video.fill")
+                    }
+                    Button(
+                        action: {
+                        print("Hello Print")
+                        }){
+                        Image(systemName: "magnifyingglass")
+                    }
+                    Button(action: {print("hello profile")}){
+                        Image("avatar").renderingMode(.original).resizable().frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    }
+                    }
+                )
+            }
+        }
+    }
+
