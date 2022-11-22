829d8a641460dce1e7a12e6bb0d48f60c8ce4d61
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-22 15:21:31 +0700

    init

---
+++ YoutubeUI/YoutubeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeUI
+//
+//  Created by azzam on 22/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YoutubeUI/YoutubeUI/YoutubeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeUIApp.swift
+//  YoutubeUI
+//
+//  Created by azzam on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
