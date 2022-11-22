738e1c05303235a6e8a70d7147a3442f91212144
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-22 18:28:51 +0700

    video list

--- YoutubeUI/YoutubeUI/ContentView.swift
+++ YoutubeUI/YoutubeUI/ContentView.swift
@@ -22,15 +22,17 @@ struct ContentView_Previews: PreviewProvider {
 struct Home : View {
     var body: some View{
         NavigationView{
-            Text("Hello ini content").navigationBarItems(
+            
+            Main()
+                .navigationBarItems(
                 leading: HStack{
                 Button(
                     action: {
                         print("Hello button")}){
-                    Image("youtube").resizable().renderingMode(.original).frame(width: 90, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Image("youtube").resizable().renderingMode(.original).frame(width: 90, height: 20)
                 }
                 },
-                trailing: HStack(spacing:20){
+                    trailing: HStack(spacing:20){
                     Button(
                         action: {
                         print("Hello Print")
@@ -52,9 +54,77 @@ struct Home : View {
                     Button(action: {print("hello profile")}){
                         Image("avatar").renderingMode(.original).resizable().frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                     }
+                }
+            )
+                .navigationBarTitle("", displayMode: .inline)
+        }
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.white/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Main : View {
+    var body: some View{
+        List {
+            VStack() {
+                ZStack (alignment: .bottomTrailing){
+                    Image("content").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    
+                    Text("10:00").padding(.all, 5).foregroundColor(.white).font(.caption).background(Color.black).cornerRadius(5).padding(.trailing, 5).padding(.bottom, 5)
+                }
+            }
+            HStack{
+                Image("avatar").resizable().frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                VStack (alignment: .leading){
+                    Text("Swift Programming Structures").font(.headline)
+                    HStack{
+                        Text("Code with Chris - 57K views - 1 day ago").font(.caption)
                     }
-                )
+                }
+                Spacer()
+                Image(systemName: "list.bullet")
+            }
+            
+            
+            VStack {
+                ZStack (alignment: .bottomTrailing){
+                    Image("content1").resizable().aspectRatio(contentMode: .fill)
+                    
+                    Text("10:00").padding(.all, 5).foregroundColor(.white).font(.caption).background(Color.black).cornerRadius(5).padding(.trailing, 5).padding(.bottom, 5)
+                }
+            }
+            HStack{
+                Image("avatar").resizable().frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                VStack (alignment: .leading){
+                    Text("BUILD FIRST SWIFTUI APP").font(.headline)
+                    HStack{
+                        Text("Code with Chris - 57K views - 1 day ago").font(.caption)
+                    }
+                }
+                Spacer()
+                Image(systemName: "list.bullet")
+            }
+            
+            VStack {
+                ZStack (alignment: .bottomTrailing){
+                    Image("content2").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    
+                    Text("10:00").padding(.all, 5).foregroundColor(.white).font(.caption).background(Color.black).cornerRadius(5).padding(.trailing, 5).padding(.bottom, 5)
+                }
+            }
+            HStack{
+                Image("avatar").resizable().frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                
+                VStack (alignment: .leading){
+                    Text("Data Flow in SwiftUI").font(.headline)
+                    HStack{
+                        Text("Code with Chris - 57K views - 1 day ago").font(.caption)
+                    }
+                }
+                Spacer()
+                Image(systemName: "list.bullet")
             }
         }
     }
-
+}
