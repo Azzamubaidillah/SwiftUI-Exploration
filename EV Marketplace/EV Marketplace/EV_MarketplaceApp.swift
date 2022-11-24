12b9a5a4eb731ada44756aee86c7bb501ba857a3
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-25 00:07:31 +0700

    ev marketplace init

---
+++ EV Marketplace/EV Marketplace/EV_MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EV_MarketplaceApp.swift
+//  EV Marketplace
+//
+//  Created by azzam on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EV_MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
