db454dd63d40f97d5b149450bcdd06dfe18d4a32
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-25 01:57:38 +0700

    model and looping card to data

--- EV Marketplace/EV Marketplace/ContentView.swift
+++ EV Marketplace/EV Marketplace/ContentView.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+//model
+
+struct ProductModel : Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+    let price : String
+    let location : String
+    
+    init(id: Int, name: String, image: String, price: String, location: String) {
+        
+        self.id = id
+        self.name = name
+        self.image = image
+        self.price = price
+        self.location = location
+        
+    }
+}
+
 struct ContentView: View {
+    
+    let data : [ProductModel] = [
+    ProductModel(id: 1, name: "Hyundai Ioniq 5", image: "ioniq-5", price:"500 M", location: "Hyundai Moto Bintaro"),
+    ProductModel(id: 2, name: "KIA EV-6", image: "ev6", price:"300 M", location: "KIA Ciputat"),
+    ProductModel(id: 3, name: "Tesla Model 3", image: "model-3", price:"780 M", location: "Tesla Singapore"),
+    ProductModel(id: 4, name: "Tesla Model X", image: "model-x", price:"700 M", location: "Tesla Singapore"),
+    ProductModel(id: 5, name: "Tesla Model Y", image: "model-y", price:"670 M", location: "Tesla Singapore"),
+    ProductModel(id: 6, name: "Tesla Model S", image: "model-s", price:"820 M", location: "Tesla Singapore"),
+    ]
+    
     var body: some View {
-        Product()
+        
+        NavigationView{
+            ScrollView{
+                ForEach(data){
+                    row in VStack(spacing: 10){
+                        Product(data: row).padding()
+                    }
+                }
+            }.navigationTitle("Electronic Vehicle")
+        }
     }
 }
 
@@ -20,23 +59,26 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Product : View {
+    
+    let data: ProductModel
+    
     var body: some View{
         
         VStack(alignment:.leading){
             ZStack(alignment: .topTrailing){
-                Image("ioniq-5").resizable().aspectRatio(contentMode: .fill).frame(height:180 ).clipped()
+                Image(self.data.image).resizable().aspectRatio(contentMode: .fill).frame(height:180 ).clipped()
                 
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                     Image(systemName: "heart").padding().foregroundColor(.red)}
                 )
             }
-            Text("Hyundai Ioniq 5").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold().padding(.leading).padding(.trailing)
+            Text(self.data.name).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold().padding(.leading).padding(.trailing)
             
-            Text("Starts from Rp. 200M").font(.caption).bold() .padding(.leading).padding(.trailing).foregroundColor(.red)
+            Text("Starts from Rp. \(self.data.price)").font(.caption).bold() .padding(.leading).padding(.trailing).foregroundColor(.red)
             
             HStack{
                 Image(systemName: "mappin.circle")
-                Text("Hyundai Moto Bintaro")}.padding(.leading).padding(.trailing).font(.caption)
+                Text(self.data.location)}.padding(.leading).padding(.trailing).font(.caption)
             
             Button(
                 action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
@@ -47,7 +89,7 @@ struct Product : View {
                             Text("Add to Cart").font(.callout).padding()
                         }
                         Spacer()
-                    }.background(Color.green).foregroundColor(.white).cornerRadius(10).padding()
+                    }.background(Color.green).foregroundColor(.white).cornerRadius(10).padding(.top, 20)
                 }
             )
         }.background(Color("light-gray")).cornerRadius(15).padding()
