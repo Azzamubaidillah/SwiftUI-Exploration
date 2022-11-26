58b1dc9ceac76446e6d9b0f6edd2fbb851b12e22
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-26 23:18:22 +0700

    init swift login

---
+++ Swift Login/Swift Login/Swift_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_LoginApp.swift
+//  Swift Login
+//
+//  Created by azzam on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
