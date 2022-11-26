ef25709d61bd0f71c1507ebc315c60074c78d6af
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-27 01:27:15 +0700

    welcome

--- Swift Login/Swift Login/ContentView.swift
+++ Swift Login/Swift Login/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Login( )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Login : View {
+    
+    @State var username : String = ""
+    @State var password : String = ""
+    
+    var body: some View{
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                HStack{
+                    HStack{
+                        VStack{
+                            Text("Hi!").bold().font(.largeTitle).foregroundColor(Color.white)
+                            
+                            Text("Welcome back!").bold().foregroundColor(Color.white)
+                        }
+                        
+                        Spacer()
+                        
+                        Image("bitmap").resizable().frame(width: 120, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding()
+                    }
+                    Spacer()
+                }.frame(height: 100).padding(30).background(Color.purple).edgesIgnoringSafeArea(.top)
+                
+                ≈
+                
+                Spacer()
+            }
+        }
     }
 }
