de38b84c6491eade9f74599385d11d48efcdafb8
Azzam <azzam.ubaidillah05@gmail.com>
2022-11-26 22:36:08 +0700

    global object

---
+++ EV Marketplace/EV Marketplace/GlobalObject.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalObject.swift
+//  EV Marketplace
+//
+//  Created by azzam on 26/11/22.
+//
+
+import Foundation
+
+class GlobalObject: ObservableObject {
+    @Published var count : Int = 0
+}
